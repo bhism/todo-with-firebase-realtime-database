3446e3e8d51b8c0ff485290d785d960494009abb
BHISM <61289719+bhism@users.noreply.github.com>
2021-12-26 12:30:58 +0530

    Add files via upload

---
+++ addnote.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class addnote extends StatelessWidget {
+  TextEditingController title = TextEditingController();
+
+  final fb = FirebaseDatabase.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    final ref = fb.ref().child('todos');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Todos"),
+        backgroundColor: Colors.indigo[900],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: title,
+                decoration: InputDecoration(
+                  hintText: 'title',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              color: Colors.indigo[900],
+              onPressed: () {
+                ref
+                    .push()
+                    .set(
+                      title.text,
+                    )
+                    .asStream();
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => Home()));
+              },
+              child: Text(
+                "save",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
